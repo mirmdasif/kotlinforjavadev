538e0d759b31ab0dfa47f1c67a0410ecaad7cf0b
Mir Md Asif Hossain <mirmdasif@gmail.com>
2020-11-08 10:29:59 +0100

    Week 2: In with range

--- src/main/kotlin/net/asifhossain/kotlinforjavadev/week2/checksandranges/ChecksForBelonging.kt
+++ src/main/kotlin/net/asifhossain/kotlinforjavadev/week2/checksandranges/ChecksForBelonging.kt
@@ -7,8 +7,18 @@ fun main() {
 
     println("a: ${isNotDigit('1')}")
     println("1: ${isLetter('1')}")
+
+    println("1: ${recognize('1')}")
+    println("b: ${recognize('b')}")
+    println("&: ${recognize('&')}")
 }
 
 fun isLetter(ch: Char) = ch in 'a'..'z' || ch in 'A'..'Z'
 
 fun isNotDigit(ch: Char) = ch !in '0'..'9'
+
+fun recognize(ch: Char) = when (ch) {
+    in '0'..'9' -> "It's a digit"
+    in 'a'..'z', in 'A'..'Z' -> "It's a letter"
+    else -> "Not alpha numeric"
+}
